d8b3d12b53a304e88336be4a65e2ae54902d979d
Kevin Le <solnovus@gmail.com>
2012-07-23 12:55:27 -0700

    sync with gelens tip

--- geventwebsocket/python_fixes.py
+++ geventwebsocket/python_fixes.py
@@ -1,5 +1,7 @@
 import sys
+from errno import EINTR
 from socket import error as socket_error
+
 
 if sys.version_info[:2] == (2, 7):
     # Python 2.7 has a working BufferedReader but socket.makefile() does not
